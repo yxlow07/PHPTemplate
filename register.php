2ee71ff26d38f8e3b82ec02d43c6a0852f47146a
yxlow07 <M-8717828@moe-dl.edu.my>
2022-05-07 20:35:39 +0800

    added reference and icons (hacked)

--- register.php
+++ register.php
@@ -1,7 +1,8 @@
 <?php
 include_once "assets/php/config.php";
 
-function pwdConf($password) {
+function pwdConf($password)
+{
     if ($pwdConfVerf = Verification::validate($_POST['pwdConf'] ?? false, [0, 1])) {
         if (Verification::checkReturns($pwdConfVerf)) {
             if ($password === $_POST['pwdConf'])
@@ -36,27 +37,32 @@ if (isset($_POST['register'])) {
             Verification::returnErrorMessage(["issetAndNotEmpty", "length" => "256"], $pwd_validation, "Password");
         }
     }
-
 }
 
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="./assets/images/2.png" type="image/x-icon">
+    <script src="./assets/js/icons.js"></script>
     <title>Register</title>
 </head>
+
 <body>
-<form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
-    <label>Email:</label> <input type="email" name="email"> <br>
-    <label>Username:</label> <input type="text" name="username"> <br>
-    <label>Password: </label> <input type="password" name="pwd"> <br>
-    <label>Confirm Password: </label> <input type="password" name="pwdConf"> <br>
-    <input type="hidden" name="register">
-    <button type="submit">Register</button>
-</form>
+    <i class="fa-duotone fa-360-degrees"></i>
+    <form action="<?php //htmlspecialchars($_SERVER['PHP_SELF'])
+                    ?>" method="post">
+        <label>Email:</label> <input type="email" name="email"> <br>
+        <label>Username:</label> <input type="text" name="username"> <br>
+        <label>Password: </label> <input type="password" name="pwd"> <br>
+        <label>Confirm Password: </label> <input type="password" name="pwdConf"> <br>
+        <input type="hidden" name="register">
+        <button type="submit">Register</button>
+    </form>
 </body>
+
 </html>
