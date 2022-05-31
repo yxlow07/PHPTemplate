8111adef9c09a59143cdd71c38a7a06aef07690e
yxlow07 <M-8717828@moe-dl.edu.my>
2022-05-31 20:51:25 +0800

    Returned original post validating

--- router.php
+++ router.php
@@ -8,10 +8,28 @@ $router = new Router(__DIR__ . "\\", "/ProjectPapa", "http://localhost/ProjectPa
 $router->GET("/", "index", ["wrap"]);
 $router->GET("/register", "register", ["wrap"]);
 $router->POST("/register", function () {
+    $errors = [];
+    $verification = new Verification();
+    if (isset($_POST['register'])) {
+
+        $email_validation = $verification->validate($_POST['email'] ?? false, "Email", ["notEmpty", ["length"], "email"]);
+        if ($email_validation !== true) {
+            $errors["email_err"] = $email_validation;
+        }
+
+        $username_validation = $verification->validate($_POST['username'] ?? false, "Username", ["notEmpty", ["length", 5, 30]]);
+        if ($username_validation !== true) {
+            $errors["username_errors"] = $username_validation;
+        }
+
+        $pwd_validation = $verification->validate($_POST['pwd'] ?? false, "Password", [ "notEmpty", ["length"], ["match", "confirmed password", $_POST['pwdConf']] ]);
+        if ($pwd_validation !== true) {
+            $errors["pwd_errors"] = ($pwd_validation);
+        }
+    }
     echo "<pre>";
-    print_r($_POST);
+    print_r($errors);
     echo "</pre>";
-    echo "This is the post request";
 });
 
 $router->run();
